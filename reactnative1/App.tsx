6129fe6d3405412eb6d6c215657fbb32836d2308
Tabindanoor <tabindanoor415@gmail.com>
2024-10-06 21:43:48 +0500

    msg

--- reactnative1/App.tsx
+++ reactnative1/App.tsx
@@ -92,13 +92,7 @@ function App(): React.JSX.Element {
           <Section title="Learn More">
             Read the docs to discover what to do next:
            
-          </Section> <Section title="Learn More">
-            Read the docs to discover what to do next:
-           
-          </Section> <Section title="Learn More">
-            Read the docs to discover what to do next:
-           
-          </Section>
+         </Section>
           <LearnMoreLinks />
         </View>
       </ScrollView>
