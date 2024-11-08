1f6f2c621452b3e3b4a0a84b2f43077c276aa9ba
Tabindanoor <tabindanoor415@gmail.com>
2024-11-08 21:11:07 +0500

    msg

--- components/NetflixCard.tsx
+++ components/NetflixCard.tsx
@@ -22,13 +22,7 @@ const NetflixCard = () => {
       />
   
       <ThemedText style={styles.title}>Netflix Show</ThemedText>
-      <Text>I want to work But And Allah will help </Text>
-      <Text>I want to to go to Germany  </Text>
-      <Text>Why i am doing that all day   </Text>
-      <Text>I am wasting my time all the day   </Text>
-      <Text>Dont know why is it happening </Text>
-      <Text>I will do it in sha Allah</Text>
-      <Text>I am deceiving my self</Text>
+    
       <ThemedText style={styles.description}>Watch the latest movies and TV shows on Netflix.</ThemedText>
       <Button title="Go to Netflix" onPress={handleButtonPress} />
     </View>
